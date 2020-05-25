f0438cb3c501e34c07106836905b5149d3d01448
Redfact <tantelyrandria@ymail.com>
2020-05-25 18:18:39 +0200

    add/rm CDN func

---
+++ script.js
@@ -0,0 +1,78 @@
+            //Elements 
+let footer = document.querySelector('footer')
+let navbarheader = document.querySelector('#navbarHeader')
+let burgerbtn = document.querySelector('.navbar-toggler')
+let card1Text = document.querySelector('p.card-text')
+let editcard1btn = document.querySelector("button.btn-outline-secondary")
+let card2Text = document.querySelectorAll('p.card-text')[1]
+let editcard2btn = document.querySelectorAll("button.btn-outline-secondary")[1]
+let stylelink = document.querySelector("link")
+let navbar = document.querySelector(".navbar")
+            //Variables 
+
+//check if navbarheader has collapse
+let haveCollapse = navbarheader.classList.contains('collapse')
+let countclick=0
+let clickedEdit2= true
+let removedlink= false
+let defaultlink = stylelink.href
+
+
+
+
+function Affclick(){
+    countclick++
+    console.log("clic numéro "+countclick)
+}
+
+function AddRemoveCollapse(){
+    if( haveCollapse) {
+        navbarheader.classList.remove('collapse');
+        haveCollapse= false ;
+    }
+    else {
+        navbarheader.classList.add('collapse');
+        haveCollapse= true ;
+    }
+}
+
+
+function ChangeTextColor(textref,color){
+    textref.style.color = color
+}
+
+function SwitchTextColor(textref){
+    if( clickedEdit2) {
+        ChangeTextColor(textref,'green');
+        clickedEdit2= false ;
+    }
+    else {
+        //  inherit is the default color from the stylsheet
+        ChangeTextColor(textref,"inherit");
+        clickedEdit2= true ;
+    }
+}
+
+function AddRemoveCDN(){
+    if( removedlink ) {
+        stylelink.href=defaultlink;
+        removedlink=false
+    }
+    else {
+        stylelink.href=" ";
+        removedlink=true;
+    }
+    console.log(stylelink.href)
+
+}
+
+
+
+
+
+burgerbtn.addEventListener('click',AddRemoveCollapse)
+footer.addEventListener('click',Affclick)
+editcard1btn.addEventListener('click',function() { ChangeTextColor(card1Text,'red'); } )
+editcard2btn.addEventListener('click',function(){ SwitchTextColor(card2Text) })
+navbar.addEventListener('dblclick',AddRemoveCDN)
+            
